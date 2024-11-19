8596f19f4bf3b5297c6c6507fc1e952a19653e91
lMatkovic <lmatkovic00@gmail.com>
2024-11-19 02:36:55 +0100

    Homework1

---
+++ GDG-Osijek/Lectures/Lectures1/Homework1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val birthYear = 2000
+    val currentYear = 2024
+
+    val currentAge = currentYear - birthYear
+
+    val ageIn2048 = 2048 - birthYear
+
+    println("Current age: $currentAge years")
+    println("Age in 2048: $ageIn2048 years")
+
+}
+
